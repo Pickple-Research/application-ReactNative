9bdf2ad7cc5658633dd09c017dbb70d2e61d2b3e
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2022-04-06 10:43:23 +0900

    (hot fix) add home stack 'n'avigator

---
+++ src/Navigator/Home.stack.navigator.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { MainHomeScreen, MainHomeScreenProps } from "@Screen/Main/MainHome";
+
+export const HomeStack = createNativeStackNavigator<HomeStackProps>();
+
+export function HomeStackNavigator() {
+  return (
+    <HomeStack.Navigator screenOptions={{ headerTitle: "" }}>
+      <HomeStack.Screen name={"MainHomeScreen"} component={MainHomeScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+export type HomeStackProps = {
+  MainHomeScreen: MainHomeScreenProps;
+};
