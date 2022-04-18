4ea950e0b58372424b4d6cb2b19d938bc647129f
cryingInitial <67569407+cryingInitial@users.noreply.github.com>
2022-04-18 20:00:40 +0900

    Flatlist Item 추가 (#29)
    Co-authored-by: 2020144057 <specific0924@yonsei.ac.kr>

--- src/Screen/Home/Home.main.screen.tsx
+++ src/Screen/Home/Home.main.screen.tsx
@@ -1,29 +1,166 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, LogBox, StyleSheet, Text, View } from "react-native";
 import { LinearGradeintContainer } from "@Component/React/index";
 import { FullView } from "@Component/StyledComponents";
 import { theme } from "@Theme/index";
-import { ResearchFlatList } from "@Component/React/ResearchFlatList.component";
 import styled from "styled-components/native";
 import { viewTheme } from "@Theme/Component";
+import { VoteListItem } from "@Component/React/Home/Vote.listitem.component";
+import { HotVoteListItem } from "@Component/React/Home/HotVote.listitem.component";
+import { ResearchListItem } from "@Component/React/Home/Research.listitem.component";
+import { Carousel } from "@Component/React/Carousel.component";
+import { vw } from "@Theme/Value";
 
 export function HomeMainScreen() {
+  LogBox.ignoreLogs([
+    'VirtualizedLists should never be nested', // TODO: Remove when fixed
+  ])
+  const toy_data = {
+    data_research: [
+      {index: 1, title: "안 사요 안사"},
+      {index: 2, title: "안 사요 안사"},
+      {index: 3, title: "안 사요 안사"},
+    ],
+    data_hot_vote: [
+      {index: 11, title: "사요 사"},
+      {index: 12, title: "사요 사"},
+      {index: 13, title: "사요 사"},
+    ],
+    data_vote: [
+      {index: 21, title: "살까말까"},
+      {index: 22, title: "살까말까"},
+      {index: 23, title: "살까말까"},
+      {index: 24, title: "살까말까"},
+      {index: 21, title: "살까말까"},
+      {index: 22, title: "살까말까"},
+      {index: 23, title: "살까말까"},
+      {index: 24, title: "살까말까"},
+      {index: 21, title: "살까말까"},
+      {index: 22, title: "살까말까"},
+      {index: 23, title: "살까말까"},
+      {index: 24, title: "살까말까"},
+      {index: 21, title: "살까말까"},
+      {index: 22, title: "살까말까"},
+      {index: 23, title: "살까말까"},
+      {index: 24, title: "살까말까"},
+      {index: 21, title: "살까말까"},
+      {index: 22, title: "살까말까"},
+      {index: 23, title: "살까말까"},
+      {index: 24, title: "살까말까"},
+    ],
+  }
+  const data = [
+    { partnerName: "스타트업1", color: "red" },
+    { partnerName: "스타트업2", color: "orange" },
+    { partnerName: "스타트업3", color: "yellow" },
+    { partnerName: "스타트업4", color: "green" },
+    { partnerName: "스타트업5", color: "blue" },
+    { partnerName: "스타트업6", color: "navy" },
+    { partnerName: "스타트업7", color: "purple" },
+  ];
   return (
     <WholeContainer>
-      <LinearGradeintContainer colors={theme.color.purple_blue_gradient}>
-        <Text>Home Main Screen - This is gradient test</Text>
-      </LinearGradeintContainer>
-      <View style={{flex: 1}}>
-        <ResearchFlatList />
+      <View style={{ flex: 1 }}>
+        <ResearchView>
+          <HeaderView>
+            <HeaderLeftText>리서치</HeaderLeftText>
+            <HeaderRightText>more</HeaderRightText>
+          </HeaderView>
+          
+          <FlatList
+            scrollEnabled={false}
+            data = {toy_data.data_research}
+            renderItem = {
+              ({ index, item }) => (
+                <ResearchListItem index item />
+              )
+            }
+          />
+        </ResearchView>
+        <HotVoteView>
+          <HeaderView>
+            <HeaderLeftText>HOT 투포</HeaderLeftText>
+            <HeaderRightText>more</HeaderRightText>
+          </HeaderView>
+          <FlatList
+            scrollEnabled={false}
+            data={toy_data.data_hot_vote}
+            renderItem = {
+              ({ index, item }) => (
+                <HotVoteListItem index item/>
+              )
+            }
+          />
+        </HotVoteView>
+        <VoteView>
+          <View
+          style = {{
+            paddingStart: 5 * vw,
+            paddingEnd: 5 * vw,
+            backgroundColor: "white",
+          }}>
+            <FlatList
+              scrollEnabled={false}
+              data={toy_data.data_vote}
+              renderItem = {
+                ({ index, item }) => (
+                  <VoteListItem index item />
+                )
+              }
+            />
+          </View>
+          
+        </VoteView>
 
       </View>
     </WholeContainer>
   );
 }
 
-export type HomeMainScreenProps = {};
+
+
+
 
 const WholeContainer = styled.ScrollView`
   width: 100%;
   background-color: ${viewTheme.home_main_background};
+`
+
+const HeaderView = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: white;
+  padding-top: 30px;
+`
+const HeaderLeftText = styled.Text`
+  color: black;
+  margin-left: ${6*vw}px;
+  font-size: 15px ;
+  font-weight: 500;
+  line-height: 20px;
+`
+
+const HeaderRightText = styled.Text`
+  color: purple;
+  margin-right: ${6*vw}px;
+  font-size: 13px;
+  font-weight: 500;
+`
+
+
+
+
+
+const ResearchView = styled.View`
+  margin-bottom: 10px;
+`
+
+
+const HotVoteView = styled.View`
+
+`
+
+const VoteView = styled.View`
+  margin-bottom: 10px;
 `
